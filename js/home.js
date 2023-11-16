fa7bcbe78740fa3d42d59b4a9b3d597a79ab2f2d
Mariclecio Amaro da Costa <mariclecioamaro@example.com>
2023-11-15 22:14:00 -0300

    coreção de código

--- js/home.js
+++ js/home.js
@@ -7,7 +7,7 @@ let data = {
 };
 
 document.getElementById("button-logout").addEventListener("click", logout);
-document.getElementById("transactions-button").addEventListener("click", function(){
+document.getElementById("transactions-button").addEventListener("click", function () {
     window.location.href = "transactions.html";
 });
 
@@ -26,9 +26,9 @@ document.getElementById("transaction-form").addEventListener("submit", function 
     });
 
     saveData(data);
-
     e.target.reset();
     myModal.hide();
+
     getCashIn();
     getCashOut();
     getTotal();
@@ -88,7 +88,7 @@ function getCashIn() {
             cashInHtml += `
            <div class="row mb-4">
                 <div class="col-12">
-                    <h3 class="fs-2">${cashIn[index].value.toFixed(2)}</h3>
+                    <h3 class="fs-2">R$ ${cashIn[index].value.toFixed(2)}</h3>
                     <div class="Container p-0">
                         <div class="row">
                             <div class="col-12 col-md-8">
@@ -106,7 +106,6 @@ function getCashIn() {
 
         document.getElementById("cash-in-list").innerHTML = cashInHtml;
     }
-
 }
 
 function getCashOut() {
